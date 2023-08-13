a01fff65874cb647af08dfb19138a20bac68b43d
FinnCummins <finnkcummins@gmail.com>
2023-08-13 20:42:04 +0100

    Completed exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const logger = require('../utils/logger')
 
 const api = supertest(app)
 
@@ -10,4 +11,9 @@ test('Blogs are returned as JSON objects', async () => {
     const blogs = await Blog.find({})
     
     expect(response.body).toHaveLength(blogs.length)
+})
+
+test('Blog post id field exists', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
 })
