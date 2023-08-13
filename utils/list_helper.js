12078be67ad1759be481f67a482a1817b7f0c842
FinnCummins <finnkcummins@gmail.com>
2023-08-13 13:35:38 +0100

    Completed exercise 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1;
 }
@@ -32,8 +34,38 @@ const favoriteBlog = (blogs) => {
     : null
 }
 
+const mostBlogs = (blogs) => {
+    let currentAuthor = null
+
+    const authors = []
+    blogs.forEach(blog => {
+        authors.push(blog.author)
+    })
+    const totalAuthorPosts = _.countBy(authors);
+
+    for (const key in totalAuthorPosts) {
+        if (currentAuthor != null) {
+            if (totalAuthorPosts[key] > currentAuthor.blogs) {
+                currentAuthor = {
+                    author: key,
+                    blogs: totalAuthorPosts[key]
+                }
+            }
+        }
+        else {
+            currentAuthor = {
+                author: key,
+                blogs: totalAuthorPosts[key]
+            }
+        }
+    }
+
+    return currentAuthor
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
