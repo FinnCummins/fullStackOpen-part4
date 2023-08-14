bff730cc9acb055202b1e3d84991291dfbfad462
FinnCummins <finnkcummins@gmail.com>
2023-08-14 22:15:16 +0100

    Completed exercise 4.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,34 +1,37 @@
-const Blog = require('../models/blog')
-const blogsRouter = require('express').Router()
-const express = require('express')
-const cors = require('cors')
-const logger = require('../utils/logger')
+const Blog = require("../models/blog");
+const blogsRouter = require("express").Router();
+const express = require("express");
+const cors = require("cors");
+const logger = require("../utils/logger");
 
-blogsRouter.use(express.json())
-blogsRouter.use(cors())
+blogsRouter.use(express.json());
+blogsRouter.use(cors());
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
-})
-  
-blogsRouter.post('/', async (request, response) => {
-    const body = request.body
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-    if (!body.hasOwnProperty("likes"))
-    {
-      body.likes = 0
-    }
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
 
-    if (!body.hasOwnProperty("title") || !body.hasOwnProperty("url"))
-    {
-      response.status(400).end()
-    }
+  if (!body.hasOwnProperty("likes")) {
+    body.likes = 0;
+  }
 
-    const blog = new Blog(body)
-  
-    const result = await blog.save()
-    response.status(201).json(result)
-  })
+  if (!body.hasOwnProperty("title") || !body.hasOwnProperty("url")) {
+    response.status(400).end();
+  } else {
+    const blog = new Blog(body);
 
-module.exports = blogsRouter
+    const result = await blog.save();
+    response.status(201).json(result);
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,63 +1,75 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
-const Blog = require('../models/blog')
-const logger = require('../utils/logger')
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blog");
+const logger = require("../utils/logger");
 
-const api = supertest(app)
+const api = supertest(app);
 
-test('Blogs are returned as JSON objects', async () => {
-    const response = await api.get('/api/blogs')
-    const blogs = await Blog.find({})
-    
-    expect(response.body).toHaveLength(blogs.length)
-})
+test("Blogs are returned as JSON objects", async () => {
+  const response = await api.get("/api/blogs");
+  const blogs = await Blog.find({});
 
-test('Blog post id field exists', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
-})
+  expect(response.body).toHaveLength(blogs.length);
+}, 10000);
 
-test('Blog post successfully created', async () => {
+test("Blog post id field exists", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body[0].id).toBeDefined();
+});
 
-    const newBlog = {
-        "title": "New Blog",
-        "author": "Steven O'Reilly",
-        "url": "stevenoreilly.com/blogs/newblog",
-        "likes": 341
-    }
-    const blogs = await Blog.find({})
-    const response = await api.post('/api/blogs').send(newBlog)
-    const getResponse = await api.get('/api/blogs')
-    expect(getResponse.body.length).toEqual(blogs.length + 1)
-})
+test("Blog post successfully created", async () => {
+  const newBlog = {
+    title: "New Blog",
+    author: "Steven O'Reilly",
+    url: "stevenoreilly.com/blogs/newblog",
+    likes: 341,
+  };
+  const blogs = await Blog.find({});
+  const response = await api.post("/api/blogs").send(newBlog);
+  const getResponse = await api.get("/api/blogs");
+  expect(getResponse.body.length).toEqual(blogs.length + 1);
+});
 
-test('Blog post created without a likes field', async () => {
-    const newBlog = {
-        "title": "New Blog",
-        "author": "Steven O'Reilly",
-        "url": "stevenoreilly.com/blogs/newblog",
-    }
-    const response = await api.post('/api/blogs').send(newBlog)
-    expect(response.body.likes).toEqual(0)
-})
+test("Blog post created without a likes field", async () => {
+  const newBlog = {
+    title: "New Blog",
+    author: "Steven O'Reilly",
+    url: "stevenoreilly.com/blogs/newblog",
+  };
+  const response = await api.post("/api/blogs").send(newBlog);
+  expect(response.body.likes).toEqual(0);
+});
 
-test('Blog post created without a title field', async () => {
-    const newBlog = {
-        "author": "Steven O'Reilly",
-        "url": "stevenoreilly.com/blogs/newblog",
-        "likes": 400
-    }
-    const response = await api.post('/api/blogs').send(newBlog)
-    expect(response.statusCode).toEqual(400)
-})
+test("Blog post created without a title field", async () => {
+  const newBlog = {
+    author: "Steven O'Reilly",
+    url: "stevenoreilly.com/blogs/newblog",
+    likes: 400,
+  };
+  await api.post("/api/blogs").send(newBlog).expect(400);
+});
 
-test('Blog post created without a url field', async () => {
-    const newBlog = {
-        "title": "New Blog",
-        "author": "Steven O'Reilly",
-        "likes": 341
-    }
-    const response = await api.post('/api/blogs').send(newBlog)
-    expect(response.statusCode).toEqual(400)
-})
+test("Blog post created without a url field", async () => {
+  const newBlog = {
+    title: "New Blog",
+    author: "Steven O'Reilly",
+    likes: 341,
+  };
+  await api.post("/api/blogs").send(newBlog).expect(400);
+});
+
+test("Deleted a blog", async () => {
+  const blogsAtTheStart = await api.get("/api/blogs");
+  const blogToDelete = blogsAtTheStart.body[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtTheEnd = await api.get("/api/blogs");
+
+  expect(blogsAtTheEnd.body).toHaveLength(blogsAtTheStart.body.length - 1);
+
+  const contents = blogsAtTheEnd.body.map((r) => r.id);
+
+  expect(contents).not.toContain(blogToDelete.id);
+});
