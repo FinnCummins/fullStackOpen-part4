152a800c26fe38774daa28731949a25a97c5a3b2
FinnCummins <finnkcummins@gmail.com>
2023-08-17 10:23:16 +0100

    Completed exercise 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,25 +1,25 @@
-const Blog = require("../models/blog");
-const blogsRouter = require("express").Router();
-const express = require("express");
-const cors = require("cors");
-const logger = require("../utils/logger");
+const Blog = require('../models/blog');
+const blogsRouter = require('express').Router();
+const express = require('express');
+const cors = require('cors');
+const logger = require('../utils/logger');
 
 blogsRouter.use(express.json());
 blogsRouter.use(cors());
 
-blogsRouter.get("/", async (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
-  if (!body.hasOwnProperty("likes")) {
+  if (!body.hasOwnProperty('likes')) {
     body.likes = 0;
   }
 
-  if (!body.hasOwnProperty("title") || !body.hasOwnProperty("url")) {
+  if (!body.hasOwnProperty('title') || !body.hasOwnProperty('url')) {
     response.status(400).end();
   } else {
     const blog = new Blog(body);
@@ -29,9 +29,26 @@ blogsRouter.post("/", async (request, response) => {
   }
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
+blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id);
   response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body;
+
+  const updatedBlog = {
+    ...body,
+    likes: body.likes,
+  };
+
+  const returned = await Blog.findByIdAndUpdate(
+    request.params.id,
+    updatedBlog,
+    { new: true }
+  );
+
+  response.json(returned);
 });
 
 module.exports = blogsRouter;
